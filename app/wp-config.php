a87a86fbaed60839bf89f3c1aa257c9688713fe6
Peter Kracik <peter@kracik.sk>
2020-05-19 14:18:11 +0200

    update formating

--- app/wp-config.php
+++ app/wp-config.php
@@ -19,17 +19,10 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') );
-
-/** MySQL database username */
-define( 'DB_USER', getenv('DB_USER') );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
-
-/** MySQL hostname */
-define( 'DB_HOST', getenv('DB_HOST') );
+define( 'DB_NAME', getenv('DB_NAME') );		// The name of the database for WordPress
+define( 'DB_USER', getenv('DB_USER') ); 	// MySQL database username
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') ); 	// MySQL database password
+define( 'DB_HOST', getenv('DB_HOST') ); 	// MySQL hostname
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
